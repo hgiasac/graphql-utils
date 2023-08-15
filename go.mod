643f58bf1e9ffcc257b7d36625929ff93300a1e0
Toan Nguyen <hgiasac@gmail.com>
2023-08-16 01:02:36 +0700

    add mock graphql client utils

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/hgiasac/graphql-utils
+
+go 1.20
+
+require github.com/hasura/go-graphql-client v0.10.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ test/mock.go
@@ -0,0 +1,154 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/hasura/go-graphql-client"
+)
+
+type MockDoType func(req *http.Request) (*http.Response, error)
+
+type MockHTTPClient struct {
+	MockDo MockDoType
+}
+
+func (m *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return m.MockDo(req)
+}
+
+// NewMockHTTPClient creates a mock client of type HTTPClient where the mock Do()
+// implementation returns the reponse and status code provided as params to the function.
+func NewMockHTTPClient(jsonResp string, statusCode int) *MockHTTPClient {
+
+	return &MockHTTPClient{
+		MockDo: func(req *http.Request) (*http.Response, error) {
+			r := io.NopCloser(bytes.NewReader([]byte(jsonResp)))
+			return &http.Response{
+				StatusCode: statusCode,
+				Body:       r,
+			}, nil
+		},
+	}
+}
+
+// NewMockGraphQLClient creates a mock graphql client of type HTTPClient where the mock Do()
+// implementation returns the response and status code provided as params to the func.
+func NewMockGraphQLClient(responses map[string]string) *graphql.Client {
+	preparedQueries := make([]string, 0, len(responses))
+	for q := range responses {
+		preparedQueries = append(preparedQueries, q)
+	}
+	return graphql.NewClient("/v1/graphql", &MockHTTPClient{
+		MockDo: func(req *http.Request) (*http.Response, error) {
+
+			bodyBytes, err := io.ReadAll(req.Body)
+			if err != nil {
+				return responseGraphQLError(err, []byte("io.ReadAll")), nil
+			}
+
+			var reqBody graphql.GraphQLRequestPayload
+			err = json.Unmarshal(bodyBytes, &reqBody)
+
+			if err != nil {
+				return responseGraphQLError(err, bodyBytes), nil
+			}
+
+			var jsonResp string
+
+			for q, resp := range responses {
+				if strings.TrimSpace(q) == strings.TrimSpace(reqBody.OperationName) ||
+					strings.TrimSpace(q) == strings.TrimSpace(reqBody.Query) {
+					jsonResp = resp
+					break
+				}
+			}
+			if jsonResp == "" {
+				return responseGraphQLError(fmt.Errorf("query not found in prepared responses: %+v", preparedQueries), bodyBytes), nil
+			}
+
+			r := io.NopCloser(bytes.NewReader([]byte(jsonResp)))
+			return &http.Response{
+				StatusCode: 200,
+				Body:       r,
+			}, nil
+		},
+	})
+}
+
+// NewMockGraphQLClientSingle creates a mock graphql client with static response
+func NewMockGraphQLClientSingle(data any, err *graphql.Error) *graphql.Client {
+	var jsonResp string
+	if sResp, ok := data.(string); ok {
+		jsonResp = sResp
+	} else {
+		jsonResp = EncodeMockGraphQLResponse(data, err)
+	}
+	return graphql.NewClient("/v1/graphql", &MockHTTPClient{
+		MockDo: func(req *http.Request) (*http.Response, error) {
+			r := io.NopCloser(bytes.NewReader([]byte(jsonResp)))
+			return &http.Response{
+				StatusCode: 200,
+				Body:       r,
+			}, nil
+		},
+	})
+}
+
+// NewMockGraphQLAffectedRowsResponse returns json response of a mutation response with affectedRows
+func NewMockGraphQLAffectedRowsResponse(mutationName string, affectedRows int) string {
+	return EncodeMockGraphQLResponse(map[string]any{
+		mutationName: map[string]int{
+			"affected_rows": affectedRows,
+		},
+	}, nil)
+}
+
+type graphqlRequestPayload struct {
+	Query         string         `json:"query"`
+	Variables     map[string]any `json:"errors"`
+	OperationName string         `json:"operationName"`
+}
+
+type graphqlResponse struct {
+	Data   any             `json:"data,omitempty"`
+	Errors []graphql.Error `json:"errors,omitempty"`
+}
+
+// EncodeMockGraphQLResponse encodes data inputs to the graphql response payload
+func EncodeMockGraphQLResponse(data any, err *graphql.Error) string {
+	resp := graphqlResponse{}
+	if data != nil {
+		resp.Data = data
+	}
+	if err != nil {
+		resp.Errors = []graphql.Error{*err}
+	}
+
+	bResp, encodeErr := json.Marshal(resp)
+	if encodeErr != nil {
+		panic(encodeErr)
+	}
+
+	return string(bResp)
+}
+
+func responseGraphQLError(err error, requestBody []byte) *http.Response {
+	reqError := EncodeMockGraphQLResponse(nil, &graphql.Error{
+		Message: err.Error(),
+		Extensions: map[string]any{
+			"code":         "validation-failed",
+			"path":         "$.selectionSet.test",
+			"request_body": string(requestBody),
+		},
+	})
+	r := io.NopCloser(bytes.NewReader([]byte(reqError)))
+	return &http.Response{
+		StatusCode: 400,
+		Body:       r,
+	}
+}
